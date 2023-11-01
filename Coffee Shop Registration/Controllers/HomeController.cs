c16eead916f8aa6492961e2f10f31377be1ee8a1
Irving Kirvan <kirvanirving@gmail.com>
2023-11-01 16:35:07 -0400

    Coffee Shop Registration Submission

---
+++ Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Coffee_Shop_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult User(User u)
+        {
+            return View(u);
+        }
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+    
+    }
+}
---
+++ Coffee Shop Registration/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Coffee_Shop_Registration.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
